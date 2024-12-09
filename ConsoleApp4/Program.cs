dd94ed8316c497be54aa26c998869129ee1f0ff5
Сардак Алексей <mamagold1213@gmail.com>
2024-12-09 17:56:07 +0200

    klass4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            Console.Write("Введите радиус круга: ");
+            double radius = double.Parse(Console.ReadLine());
+
+            CalculateCircleProperties(radius);
+            Console.ReadKey();
+        }
+        static void CalculateCircleProperties(double radius)
+        {
+            double area = Math.PI * radius * radius;
+            double circumference = 2 * Math.PI * radius;
+
+            Console.WriteLine($"Радиус: {radius}");
+            Console.WriteLine($"Площадь круга: {area}");
+            Console.WriteLine($"Длина окружности: {circumference}");
+        }
+    }
+}
